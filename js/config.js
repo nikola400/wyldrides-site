08b2793977ace9a557fb75838fd330b1bf755235
Nikola Petrov <32712213+nikola400@users.noreply.github.com>
2021-07-06 20:23:43 +0300

    Add Cognito configurations in the web app

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_0WtX1frsQ', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7ei6k2aifi6ekh0ol13eq3hodb', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
